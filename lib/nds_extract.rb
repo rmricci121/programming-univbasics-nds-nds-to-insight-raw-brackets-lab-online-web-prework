36a6815b57b938c7c3dab2893818f58ead4f963a
Robert M Ricci <rmricci121@gmail.com>
2019-11-04 14:22:12 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -22,7 +22,7 @@ def directors_totals(nds)
   # Be sure to return the result at the end!
   index = 0 
   grand_total = {} 
-  movies = directors_database[0][0]
+  movies = directors_database[index][:name]
   while index < movies.length  do 
     puts grand_total = movies[index][:worldwide_gross]
     index += 1
