064ef90ecc3caffa6025aeb7954a96091ce3fd9b
Robert M Ricci <rmricci121@gmail.com>
2019-11-04 14:59:38 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,6 +1,8 @@
+require "pry"
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 
+pp directors_database
 def directors_totals(nds)
   # Remember, it's always OK to pretty print what you get *in* to make sure
   # that you know what you're starting with!
@@ -18,5 +20,25 @@ def directors_totals(nds)
   #
   #
   # Be sure to return the result at the end!
-  nil
+  #movies = directors_database[index][:name]
+  #while index < movies.length  do 
+    # puts grand_total = movies[index][:name]
+    # index += 1
+    # end
+  directors_index = 0 
+  grand_total = {} 
+  while directors_index < nds.length do
+      directors_name = nds[directors_index][:name]
+      grand_total[directors_name] = 0  
+      movies = nds[directors_index][:movies]
+      movies_index = 0 
+    while movies_index < movies.length do
+  #binding.pry
+     grand_total[directors_name] += movies[movies_index][:worldwide_gross]
+      movies_index += 1
+    end
+    directors_index += 1
+  end
+
+  return grand_total
 end
